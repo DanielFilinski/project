b90a3c7885ee2ed14cc32ad1ce39230373d39bc8
FilinSky <daniel.filinski@gmail.com>
2022-06-09 10:08:36 +0300

    scroll 2.0

--- #42 scrollTop/src/js/script.js
+++ #42 scrollTop/src/js/script.js
@@ -1,7 +1,7 @@
 console.log('Start')
 
 const html = document.querySelector('html');
-const btn = document.querySelector('button');
+const btn = document.querySelectorAll('button');
 
 const width = html.scrollWidth;
 const height = html.scrollHeight;
@@ -9,12 +9,11 @@ const scrollTop = html.scrollTop;
 // const width = html.clientWidth;
 // const height = html.clientHeight;
 
-btn.addEventListener('click', () => {
-    console.log(html.scrollTop);
-    progress.style.width = `${html.scrollTop}px`;
+btn.forEach((item) => {
+    item.addEventListener('click', () => {
+        progress.style.width = `${(html.scrollTop / (html.scrollHeight - html.clientHeight)) * 100}%`;
+        console.log(progress.style.width);
+    });
 });
 
 const progress = document.querySelector('.progress');
-
-
-
