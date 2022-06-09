e19161de878f492606616a76087ee3b86ee12da0
FilinSky <daniel.filinski@gmail.com>
2022-06-09 11:09:15 +0300

    scroll 3.0 (динамическоем перемещение c помощью CSS transition: .5s ease-in-out )

--- #42 scrollTop/src/js/script.js
+++ #42 scrollTop/src/js/script.js
@@ -9,11 +9,24 @@ const scrollTop = html.scrollTop;
 // const width = html.clientWidth;
 // const height = html.clientHeight;
 
-btn.forEach((item) => {
-    item.addEventListener('click', () => {
-        progress.style.width = `${(html.scrollTop / (html.scrollHeight - html.clientHeight)) * 100}%`;
-        console.log(progress.style.width);
-    });
+html.addEventListener("wheel", (e) => {
+    progress.style.width = `${(html.scrollTop / (html.scrollHeight - html.clientHeight)) * 100}%`;
+    console.log(progress.style.width);
+    console.log(`e.deltaY: ${e.deltaY}`);
+    console.log(`e.deltaX: ${e.deltaX}`);
+    console.log(`e.deltaMode: ${e.deltaMode}`);
+    console.log(`e.detail: ${e.detail}`);
+    console.log(`e.wheelDelta: ${e.wheelDelta}`);
+
+
 });
+
+
+// btn.forEach((item) => {
+//     item.addEventListener('click', () => {
+//         progress.style.width = `${(html.scrollTop / (html.scrollHeight - html.clientHeight)) * 100}%`;
+//         console.log(progress.style.width);
+//     });
+// });
 
 const progress = document.querySelector('.progress');
