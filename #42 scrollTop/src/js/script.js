e37929f8ccab89b79209e50923ad673baff46fd5
FilinSky <daniel.filinski@gmail.com>
2022-06-09 12:32:26 +0300

    scroll 3.0 (динамическоем перемещение c помощью CSS transition: .5s ease-in-out )

--- #42 scrollTop/src/js/script.js
+++ #42 scrollTop/src/js/script.js
@@ -2,31 +2,10 @@ console.log('Start')
 
 const html = document.querySelector('html');
 const btn = document.querySelectorAll('button');
+const progress = document.querySelector('.progress');
 
-const width = html.scrollWidth;
-const height = html.scrollHeight;
-const scrollTop = html.scrollTop;
-// const width = html.clientWidth;
-// const height = html.clientHeight;
 
 html.addEventListener("wheel", (e) => {
-    progress.style.width = `${(html.scrollTop / (html.scrollHeight - html.clientHeight)) * 100}%`;
-    console.log(progress.style.width);
-    console.log(`e.deltaY: ${e.deltaY}`);
-    console.log(`e.deltaX: ${e.deltaX}`);
-    console.log(`e.deltaMode: ${e.deltaMode}`);
-    console.log(`e.detail: ${e.detail}`);
-    console.log(`e.wheelDelta: ${e.wheelDelta}`);
-
-
+    progress.style.width = `${((html.scrollTop / (html.scrollHeight - html.clientHeight)) * 100) - 2}%`;
+    progress.style.transition = '.2s ease-in-out';
 });
-
-
-// btn.forEach((item) => {
-//     item.addEventListener('click', () => {
-//         progress.style.width = `${(html.scrollTop / (html.scrollHeight - html.clientHeight)) * 100}%`;
-//         console.log(progress.style.width);
-//     });
-// });
-
-const progress = document.querySelector('.progress');
